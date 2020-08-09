777701eaa6c1009635360795c43c6ed743fe54f0
Utkarsh Kushagra <40884732+UK-17@users.noreply.github.com>
2020-08-09 15:21:20 +0530

    Update href.py

--- href.py
+++ href.py
@@ -1,10 +1,19 @@
+#import the libraries
 import urllib.request , urllib.parse,urllib.error
 from bs4 import BeautifulSoup
+import ssl
 
+#ignore the ssl error
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+#open URL and parse it
 url = input('URL:')
-html = urllib.request.urlopen(url).read()
+html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html,'html.parser')
 
+# look for tags
 tags = soup('a')
 for tag in tags:
   print(tag.get('href',None))
