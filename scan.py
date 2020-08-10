11c3cb4873cf712f4574288a77b1679fbeb46068
Utkarsh Kushagra <40884732+UK-17@users.noreply.github.com>
2020-08-10 18:18:45 +0530

    Update scan.py

--- scan.py
+++ scan.py
@@ -1,5 +1,5 @@
 import nmap
-scanner = nmap.PortScanner() #our scanner
+scanner = nmap.PortScanner()
 
 #input file
 input_file = 'input.txt'
@@ -9,33 +9,23 @@ input_handle = open(input_file,'r')
 output_file = 'active.txt'
 output_handle = open(output_file,'a+')
 
-count = 0
+count = 0 
 active = 0
 
 for line in input_handle: #pickup the list of urls
   
   if line.startswith('http'):  #taking only the urls
-    count+=1
-    
     line = line.split()[0] #splitting the metadata
     url = line.split('/')[2] #removing the http tags
     temp = url.split(':') #extracting the ip
     ip = temp[0]  #IP Address
     try:
-       port = temp[1] #port
+      scanner.scan(hosts=ip,arguments='-sn') #chech if host is up
+      count+=1
+      print(ip+' is ' +scanner[ip].state())
+      active+=1
+      output_handle.write(url+'\n') #write active hosts to output
     except:
-       port = 80  #port 80 by default
-    
-    try:
-     res = scanner.scan(ip,port) #scan ip from list on port
-     if scanner[ip].state() == 'up' : #check if host is up
-       active+=1
-       output_handle.write(url+'\n') #write the url
-       print(active+' hosts found\n')
-     
-    except:
-     continue
-    
-print('\nProcessed',count,'entries.\n')
-input_handle.close()
-output_handle.close()
+      continue
+print('Scanned:',count)
+print('Active:',active)
